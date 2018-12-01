5af463de248ffde2e9e9ca3e1694497cb5f9acd9
wesleykruger@gmail.com <wesleykruger@gmail.com>
2018-12-01 13:22:30 -0600

    deleted maps and upgrade components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,9 +15,7 @@ import { UserProfileComponent } from './user-profile/user-profile.component';
 import { TableListComponent } from './table-list/table-list.component';
 import { TypographyComponent } from './typography/typography.component';
 import { IconsComponent } from './icons/icons.component';
-import { MapsComponent } from './maps/maps.component';
 import { NotificationsComponent } from './notifications/notifications.component';
-import { UpgradeComponent } from './upgrade/upgrade.component';
 import {
   AgmCoreModule
 } from '@agm/core';
@@ -34,10 +32,7 @@ import { HttpClientModule } from '@angular/common/http';
     ComponentsModule,
     RouterModule,
     HttpClientModule,
-    AppRoutingModule,
-    AgmCoreModule.forRoot({
-      apiKey: 'YOUR_GOOGLE_MAPS_API_KEY'
-    })
+    AppRoutingModule
   ],
   declarations: [
     AppComponent,
